9cbd35ac0e030f49fe7bc8b4f02f4bafb2ddb9cd
dam116 <dam116@SAGRES11.olimpia.ga>
2017-05-18 10:00:37 +0200

    6

--- Boletin17-JUNIT/src/boletin17/Xeometria.java
+++ Boletin17-JUNIT/src/boletin17/Xeometria.java
@@ -1,5 +1,12 @@
+package boletin17;
 
 public class Xeometria {
+	
+	/**
+	 * Recibe o valor do lado e calcula o perímetro do cadrado
+	 * @param lado
+	 * @return
+	 */
 
 	int perimetroCadrado(int lado) {
 
@@ -8,6 +15,12 @@ public class Xeometria {
 
 		return lado * 4;
 	}
+	
+	/**
+	 * Recibe o valor do lado e calcula a área do cadrado
+	 * @param lado
+	 * @return
+	 */
 
 	int areaCadrado(int lado) {
 
@@ -16,6 +29,13 @@ public class Xeometria {
 
 		return lado * lado;
 	}
+	
+	/**
+	 * Recibe os valores dos catetos A e B de un triángulo e calcula a súa hipotenusa
+	 * @param catetoA
+	 * @param catetoB
+	 * @return
+	 */
 
 	double teoremaPitagoras(int catetoA, int catetoB) {
 		int h;
@@ -29,6 +49,13 @@ public class Xeometria {
 		return Math.sqrt(h);
 
 	}
+	
+	/**
+	 * Recibe o valor de un cateto e o valor da hipotenusa e calcula o valor do cateto restante
+	 * @param hipotenusa
+	 * @param catetoA
+	 * @return
+	 */
 
 	double teoremaPitagoras(double hipotenusa, double catetoA) {
 
